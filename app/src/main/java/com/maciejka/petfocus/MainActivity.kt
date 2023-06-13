2d91360565f26bbd311ca93faec0c2d747ec4eb0
Joanna49 <60232951+Joanna49@users.noreply.github.com>
2023-06-14 01:14:29 +0200

    Wykonano menu glowne

--- app/src/main/java/com/maciejka/petfocus/MainActivity.kt
+++ app/src/main/java/com/maciejka/petfocus/MainActivity.kt
@@ -2,10 +2,31 @@ package com.maciejka.petfocus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.maciejka.petfocus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPomodoro.setOnClickListener {
+            pokazFragment(PomodoroFragment())
+        }
+        binding.btnTodo.setOnClickListener {
+            pokazFragment(ToDoFragment())
+        }
+        binding.btnZwierzak.setOnClickListener{
+            pokazFragment(ToDoFragment())
+        }
+    }
+
+    private fun pokazFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.replace(R.id.srodkowyKontener,fragment,null)
+        fragmentTransaction.commit()
     }
 }
